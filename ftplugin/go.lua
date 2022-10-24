bcedd817be40316da9ceac157b49038db1e8e177
RitchielRez <ritchielrez@gmail.com>
2022-10-24 17:29:06 +0600

    feat(nvim-dap): add go support again

---
+++ ftplugin/go.lua
@@ -0,0 +1,13 @@
+local dap_go_ok, dap_go = pcall(require, 'dap-go')
+if not dap_go_ok then
+	return
+end
+
+dap_go.setup()
+
+-- Extra debugging keymap for go
+local opts = { noremap = true, silent = true }
+
+local map = vim.keymap.set
+
+map('n', '<Leader>dT', ':lua require"dap-go".debug_test()<CR>', opts)
