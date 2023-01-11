5d4e5507d4b608f71a46b901c0050be6148684df
RitchielRez <ritchielrez@gmail.com>
2023-01-11 17:46:16 +0600

    fix(default): indetation set to 2 spaces

--- lua/defaults/options.lua
+++ lua/defaults/options.lua
@@ -5,9 +5,9 @@ local let = vim.g
 
 set.number = true
 
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
 set.expandtab = true
 set.hlsearch = false
 set.incsearch = true
