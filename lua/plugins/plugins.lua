b5bbd96545740146985ede22884d2908fede4512
RitchielRez <ritchielrez@gmail.com>
2023-04-10 13:12:38 +0600

    fix(gitsigns): you have to toggle line blame

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -113,7 +113,7 @@ return packer.startup(function(use)
 				print('Gitsigns plugin not installed')
 			end
 			gitsigns.setup({
-				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
 				current_line_blame_opts = {
 					virt_text = true,
 					virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
