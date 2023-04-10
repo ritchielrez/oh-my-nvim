4c8825d8f20c46e572765a7fffec1f2b88a59abd
RitchielRez <ritchielrez@gmail.com>
2023-04-10 13:24:25 +0600

    feat(null_ls): add gitsigns code action support

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -1,12 +1,14 @@
 local null_ls_ok, null_ls = pcall(require, 'null-ls')
 if not null_ls_ok then
-    print('Null-ls plugin not installed')
+	print('Null-ls plugin not installed')
 end
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.goimports,
-        null_ls.builtins.formatting.stylua,
-    },
+	sources = {
+		null_ls.builtins.formatting.clang_format,
+		null_ls.builtins.formatting.goimports,
+		null_ls.builtins.formatting.stylua,
+
+		null_ls.builtins.code_actions.gitsigns,
+	},
 })
