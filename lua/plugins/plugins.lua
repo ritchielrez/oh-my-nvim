b0887cb7b7bb12255ea89aafb6ee6e7bbfe84dd9
RitchielRez <ritchielrez@gmail.com>
2023-04-16 12:28:50 +0600

    feat(plugins): lspconfig is dependent on mason.nvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -53,7 +53,10 @@ require('lazy').setup({
 	{
 		'williamboman/mason.nvim',
 		event = 'VeryLazy',
-		dependencies = 'williamboman/mason-lspconfig.nvim',
+		dependencies = {
+			'williamboman/mason-lspconfig.nvim',
+			'neovim/nvim-lspconfig',
+		},
 		config = function()
 			require('lsp.lsp')
 		end,
