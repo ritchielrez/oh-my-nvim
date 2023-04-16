ccb800358ca6128f2489eaeb6be1c71846770fe1
RitchielRez <ritchielrez@gmail.com>
2023-04-16 16:04:15 +0600

    fix(lspsaga): disable floating window title

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -69,7 +69,7 @@ require('lazy').setup({
 			request_timeout = 2000,
 			ui = {
 				-- This option only works in Neovim 0.9
-				title = true,
+				title = false,
 				-- Border type can be single, double, rounded, solid, shadow.
 				border = 'single',
 				winblend = 0,
