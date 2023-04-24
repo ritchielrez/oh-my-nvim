f8b0ca23a490a4d75efd8ea2c45d93a553511183
RitchielRez <ritchielrez@gmail.com>
2023-04-24 14:23:27 +0600

    feat(null-ls): add yamlfmt support

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -8,6 +8,7 @@ null_ls.setup({
 		null_ls.builtins.formatting.clang_format,
 		null_ls.builtins.formatting.goimports,
 		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.yamlfmt,
 
 		null_ls.builtins.code_actions.gitsigns,
 	},
