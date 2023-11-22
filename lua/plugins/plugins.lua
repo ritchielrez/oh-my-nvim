05b63abe1457a2bea6d2ae43f63d9eb8fab5fec7
RitchielRez <ritchielrez@gmail.com>
2023-11-23 01:05:56 +0600

    feat(plugins): add conform.nvim
    replacing null-ls which is right now an archived plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -59,6 +59,28 @@ require('lazy').setup({
 		end,
 	},
 
+	-- Formatting support
+	{
+		'stevearc/conform.nvim',
+		event = 'BufWritePre',
+		cmd = { 'ConformInfo' },
+		keys = {
+			{
+				'<leader>lf',
+				function()
+					require('conform').format({ async = true, lsp_fallback = true })
+				end,
+				mode = '',
+				desc = 'Format buffer',
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { 'stylua' },
+			},
+		},
+	},
+
 	-- Diagnostics support
 	{
 		'mfussenegger/nvim-lint',
