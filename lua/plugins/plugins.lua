ababd74f8297784da397b02859545273559d58ea
RitchielRez <ritchielrez@gmail.com>
2023-11-28 15:46:49 +0600

    chore(markdown-preview): enable lazy loading

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -247,6 +247,8 @@ local plugins = {
 	----- Markdown
 	{
 		'iamcco/markdown-preview.nvim',
+		cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+		ft = { 'markdown' },
 		build = function()
 			vim.fn['mkdp#util#install']()
 		end,
