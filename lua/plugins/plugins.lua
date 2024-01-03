996548b0075c127bc4dd4433c402fe3618a8fca2
RitchielRez <ritchielrez@gmail.com>
2024-01-03 15:58:10 +0600

    chore(alpha): use a custom theme
    The theme is originally authored by @AdamWhittingham

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -40,7 +40,7 @@ local plugins = {
 		'goolord/alpha-nvim',
 		event = 'VimEnter',
 		config = function()
-			require('alpha').setup(require('alpha.themes.dashboard').config)
+			require('alpha').setup(require('plugins.alpha').config)
 		end,
 	},
 
