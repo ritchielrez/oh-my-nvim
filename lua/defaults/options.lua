4b3e9902018e7922e6e92a4ce161bfe8af4b6d72
RitchielRez <ritchielrez@gmail.com>
2024-01-06 01:26:30 +0600

    chore(opts): enable `neovide_unlink_border_highlights`

--- lua/defaults/options.lua
+++ lua/defaults/options.lua
@@ -65,3 +65,5 @@ vim.g.neovide_padding_top = 20
 vim.g.neovide_padding_bottom = 20
 vim.g.neovide_padding_right = 20
 vim.g.neovide_padding_left = 20
+
+vim.g.neovide_unlink_border_highlights = true
