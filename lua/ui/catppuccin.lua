13ffbd3604982414a02d246a67afd38e79ded4d0
RitchielRez <ritchielrez@gmail.com>
2024-01-10 01:22:29 +0600

    feat!(catppuccin): remove old setup
    `catppuccin` nvim colorscheme has updated
    setup function table, so I updated it.

--- lua/ui/catppuccin.lua
+++ lua/ui/catppuccin.lua
@@ -12,21 +12,25 @@ if not catppuccin_status_ok then
 end
 
 catppuccin.setup({
-	no_italic = true,
-	dim_inactive = {
-		enabled = false,
-		shade = 'dark',
-		percentage = 0.15,
+	flavour = 'mocha', -- latte, frappe, macchiato, mocha
+	background = { -- :h background
+		light = 'latte',
+		dark = 'mocha',
 	},
 	transparent_background = transparent_background,
-	term_colors = true,
-	compile = {
-		enabled = false,
-		path = vim.fn.stdpath('cache') .. '/catppuccin',
+	show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+	term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+	dim_inactive = {
+		enabled = false, -- dims the background color of inactive window
+		shade = 'dark',
+		percentage = 0.15, -- percentage of the shade to apply to the inactive window
 	},
-	styles = {
-		comments = {},
-		conditionals = {},
+	no_italic = false, -- Force no italic
+	no_bold = false, -- Force no bold
+	no_underline = false, -- Force no underline
+	styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+		comments = { 'italic' }, -- Change the style of comments
+		conditionals = { 'italic' },
 		loops = {},
 		functions = {},
 		keywords = {},
@@ -38,72 +42,24 @@ catppuccin.setup({
 		types = {},
 		operators = {},
 	},
-	integrations = {
-		treesitter = true,
-		native_lsp = {
-			enabled = true,
-			virtual_text = {
-				errors = {},
-				hints = {},
-				warnings = {},
-				information = {},
-			},
-			underlines = {
-				errors = { 'undercurl' },
-				hints = { 'undercurl' },
-				warnings = { 'undercurl' },
-				information = { 'undercurl' },
-			},
+	color_overrides = {
+		mocha = {
+			base = '#181825',
 		},
-		coc_nvim = false,
-		lsp_trouble = false,
-		cmp = true,
-		lsp_saga = false,
-		gitgutter = false,
-		gitsigns = true,
-		leap = false,
-		telescope = true,
-		nvimtree = {
-			enabled = true,
-			show_root = true,
-			transparent_panel = false,
-		},
-		neotree = {
-			enabled = false,
-			show_root = true,
-			transparent_panel = false,
-		},
-		dap = {
-			enabled = false,
-			enable_ui = false,
-		},
-		which_key = false,
-		indent_blankline = {
-			enabled = true,
-			colored_indent_levels = false,
-		},
-		dashboard = true,
-		neogit = false,
-		vim_sneak = false,
-		fern = false,
-		barbar = false,
-		bufferline = true,
-		markdown = true,
-		lightspeed = false,
-		ts_rainbow = false,
-		hop = false,
-		notify = true,
-		telekasten = true,
-		symbols_outline = true,
-		mini = false,
-		aerial = false,
-		vimwiki = true,
-		beacon = true,
-		navic = false,
-		overseer = false,
 	},
-	color_overrides = {},
-	highlight_overrides = {},
+	custom_highlights = {},
+	integrations = {
+		cmp = true,
+		gitsigns = true,
+		nvimtree = true,
+		treesitter = true,
+		notify = true,
+		mini = {
+			enabled = true,
+			indentscope_color = '',
+		},
+		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+	},
 })
 
 require('ui.color')
