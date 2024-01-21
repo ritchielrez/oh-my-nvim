495ebdc44bb5d69c257b45fbf3222adad7b914df
RitchielRez <ritchielrez@gmail.com>
2024-01-22 01:01:01 +0600

    fix(debugprint): remove extra comments

--- lua/plugins/debugprint.lua
+++ lua/plugins/debugprint.lua
@@ -21,19 +21,16 @@ end, {
 	expr = true,
 })
 vim.keymap.set('n', '<Leader>dq', function()
-	-- Note: setting `expr=true` and returning the value are essential
 	return require('debugprint').debugprint({ variable = true })
 end, {
 	expr = true,
 })
 vim.keymap.set('n', '<Leader>Dq', function()
-	-- Note: setting `expr=true` and returning the value are essential
 	return require('debugprint').debugprint({ above = true, variable = true })
 end, {
 	expr = true,
 })
 vim.keymap.set('n', '<Leader>do', function()
-	-- Note: setting `expr=true` and returning the value are essential
 	-- It's also important to use motion = true for operator-pending motions
 	return require('debugprint').debugprint({ motion = true })
 end, {
