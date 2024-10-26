ee27b77f2470c1c2892133347c51cc39816a4a6f
RitchielRez <ritchielrez@gmail.com>
2024-10-25 20:50:48 -0400

    chore(keymaps): change tab width to 2 spaces

--- lua/defaults/options.lua
+++ lua/defaults/options.lua
@@ -8,9 +8,9 @@ set.relativenumber = true
 
 set.linespace = 0
 
-set.tabstop = 4
-set.softtabstop = 4
-set.shiftwidth = 4
+set.tabstop = 2
+set.softtabstop = 2
+set.shiftwidth = 2
 set.expandtab = true
 set.hlsearch = false
 set.incsearch = true
