694a8dc375835948051b6e01324fc1dcbffd8c3d
RitchielRez <ritchielrez@gmail.com>
2024-11-02 23:37:18 -0400

    chore(plugins): remove telescope-file-browser

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -90,7 +90,6 @@ local plugins = {
 		dependencies = {
 			'nvim-telescope/telescope-project.nvim',
 			'nvim-telescope/telescope-ui-select.nvim',
-			'nvim-telescope/telescope-file-browser.nvim',
 			{
 				'nvim-telescope/telescope-fzf-native.nvim',
 				build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build',
