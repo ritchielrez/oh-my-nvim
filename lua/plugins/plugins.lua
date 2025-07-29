dbb23188a99bc90763b57a5c0c0598e1d20c3172
RitchielRez <ritchielrez@gmail.com>
2025-07-29 12:48:18 -0400

    refactor(plugins): lock cmp-nvim-lsp to latest commit

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -208,6 +208,7 @@ local plugins = {
 	},
 	{
 		'hrsh7th/cmp-nvim-lsp',
+		commit = 'a8912b88ce488f411177fc8aed358b04dc246d7b',
 		event = 'LspAttach',
 		dependencies = {
 			'hrsh7th/cmp-nvim-lsp-signature-help',
