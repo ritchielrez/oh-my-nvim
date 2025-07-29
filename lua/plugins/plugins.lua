39e6648b721c50c02adb846ef4f14d2379d6c06d
RitchielRez <ritchielrez@gmail.com>
2025-07-29 12:44:48 -0400

    refactor(plugins): use latest stable version for catppuccin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -21,6 +21,7 @@ local plugins = {
 	-- 'tiagovla/tokyodark.nvim',
 	{
 		'catppuccin/nvim',
+		version = '*',
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		name = 'catppuccin',
