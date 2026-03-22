664f5fcfcae133305daefa1eeb3e0cf998b1a58b
RitchielRez <ritchielrez@gmail.com>
2026-03-21 23:02:55 -0400

    fix: comment unused `set_transparent()` function definition

--- init.lua
+++ init.lua
@@ -1,26 +1,26 @@
 vim.opt.termguicolors = true
 
-local function set_transparent() -- set UI component to transparent
-	local groups = {
-		'Normal',
-		'NormalNC',
-		'EndOfBuffer',
-		'NormalFloat',
-		'FloatBorder',
-		'SignColumn',
-		'StatusLine',
-		'StatusLineNC',
-		'TabLine',
-		'TabLineFill',
-		'TabLineSel',
-		'ColorColumn',
-	}
-	for _, g in ipairs(groups) do
-		vim.api.nvim_set_hl(0, g, { bg = 'none' })
-	end
-	vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none', fg = '#767676' })
-end
-
+-- local function set_transparent() -- set UI component to transparent
+-- 	local groups = {
+-- 		'Normal',
+-- 		'NormalNC',
+-- 		'EndOfBuffer',
+-- 		'NormalFloat',
+-- 		'FloatBorder',
+-- 		'SignColumn',
+-- 		'StatusLine',
+-- 		'StatusLineNC',
+-- 		'TabLine',
+-- 		'TabLineFill',
+-- 		'TabLineSel',
+-- 		'ColorColumn',
+-- 	}
+-- 	for _, g in ipairs(groups) do
+-- 		vim.api.nvim_set_hl(0, g, { bg = 'none' })
+-- 	end
+-- 	vim.api.nvim_set_hl(0, 'TabLineFill', { bg = 'none', fg = '#767676' })
+-- end
+--
 -- set_transparent()
 
 -- ============================================================================
