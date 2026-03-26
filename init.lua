f39989d79d1b51cb697f3227c203a67da17fe0a5
RitchielRez <ritchielrez@gmail.com>
2026-03-25 22:19:31 -0400

    feat: add diagnostic config

--- init.lua
+++ init.lua
@@ -492,3 +492,38 @@ local opts = {
 }
 
 require('lazy').setup(plugins, opts)
+
+-- ============================================================================
+-- DIAGNOSITC
+-- ============================================================================
+local config = {
+	-- virtual_lines = true,
+	-- virtual_text = false,
+	-- virtual_text = {
+	-- 	-- spacing = 7,
+	-- 	-- update_in_insert = false,
+	-- 	-- severity_sort = true,
+	-- 	-- prefix = "<-",
+	-- 	prefix = ' ●',
+	-- 	source = 'if_many', -- Or "always"
+	-- 	-- format = function(diag)
+	-- 	--   return diag.message .. "blah"
+	-- 	-- end,
+	-- },
+
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = true,
+		style = 'minimal',
+		border = 'none',
+		-- border = {"▄","▄","▄","█","▀","▀","▀","█"},
+		source = 'if_many', -- Or "always"
+		header = '',
+		prefix = '',
+		-- width = 40,
+	},
+}
+
+vim.diagnostic.config(config)
