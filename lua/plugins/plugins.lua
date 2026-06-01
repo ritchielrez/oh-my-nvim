11d65937069053ea312c59815db70d4b6fc8f777
RitchielRez <ritchielrez@gmail.com>
2026-06-01 11:53:41 -0400

    feat(plugins): add `decisive.nvim`

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -156,6 +156,44 @@ local plugins = {
 		end,
 	},
 
+	{
+		'emmanueltouzery/decisive.nvim',
+		config = function()
+			require('decisive').setup({})
+		end,
+		ft = { 'csv' },
+		keys = {
+			{
+				'<leader>cca',
+				":lua require('decisive').align_csv({})<cr>",
+				{ silent = true },
+				desc = 'Align CSV',
+				mode = 'n',
+			},
+			{
+				'<leader>ccA',
+				":lua require('decisive').align_csv_clear({})<cr>",
+				{ silent = true },
+				desc = 'Align CSV clear',
+				mode = 'n',
+			},
+			{
+				'[c',
+				":lua require('decisive').align_csv_prev_col()<cr>",
+				{ silent = true },
+				desc = 'Align CSV prev col',
+				mode = 'n',
+			},
+			{
+				']c',
+				":lua require('decisive').align_csv_next_col()<cr>",
+				{ silent = true },
+				desc = 'Align CSV next col',
+				mode = 'n',
+			},
+		},
+	},
+
 	-- Formatting and diagnostics support
 	{
 		'stevearc/conform.nvim',
